5a5776b51ac266339398b5822b1164686a9ce1d9
Nigelmarowa <53082722+Nigelmarowa@users.noreply.github.com>
2019-08-03 17:10:35 +0200

    Update template.py
    GPIO event detection update

--- EEE3096S/Prac1/template.py
+++ EEE3096S/Prac1/template.py
@@ -21,7 +21,9 @@ outputpin = [13, 31, 37]
 inputpin = [15, 29] 
 
 # outputs showing the list for the 3-bit counter
-listCounter =list(product([0,1], repeat=3)) 
+
+listCounter =list(product([0,1], repeat=3))
+
 #This is where the main function starts
 def main():
     #This line shows a counter variable going to be used
@@ -35,3 +37,34 @@ def main():
     function1 = GPIO.gpio_function(33)
     
     print(function1)
+    
+    while True:
+
+        if GPIO.event_detected(29): #incrementing the LEDs by 1
+            
+        counter=counter + 1
+        
+        if counter ==8:
+            
+            counter=0
+
+        print(counterList[counter])
+
+        GPIO.output(outputpin, listCounter[counter])
+        
+        sleep(.2)
+        
+        if GPIO.event_detected(31):  #decrementing the LEDs by 1
+
+            counter-=1
+
+            if counter ==-1:
+                
+                counter=7
+                
+            print(listCounter[counter])
+            
+            GPIO.output(outputpin, listCounter[counter])
+            
+            sleep(.2)
+        sleep(2)    
